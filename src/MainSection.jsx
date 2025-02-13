1202b940d1a0690a63c86b9970c5185dcf6223fc
dror1 <dror@ardon.co.il>
2025-02-13 12:35:42 +0200

    added my old files

---
+++ src/MainSection.jsx
@@ -0,0 +1,30 @@
+import "./MainSection.css";
+
+export default function MainSection() {
+  return (
+    <section>
+      <header>
+        <nav>
+          <ul class="nav-links">
+            <li>
+              <div class="logo">
+                <span class="dev">DEV</span>
+                <span class="name">DROR KRIEF</span>
+              </div>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <h1>hello world</h1>
+    </section>
+  );
+}
