c543a27e78b15efdb7131f2f65b48f539bb3d75d
dror1 <dror@ardon.co.il>
2025-02-13 12:10:05 +0200

    :q q q: X X: :X Q: :Q

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dror-home-page/',
+  base: "/dror-home-page/",
 })
